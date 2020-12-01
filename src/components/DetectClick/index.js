423fe9b92a01d6f070126c5f93d36224e6cfd6e8
nguynhieu <tthzicc@gmail.com>
2020-12-01 20:51:09 +0700

    fix some code

--- src/components/DetectClick/index.js
+++ src/components/DetectClick/index.js
@@ -1,26 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 
-const useOutsideAlerter = (ref, cb) => {
-  useEffect(() => {
-    // handle if clicked on outside of element
-    function handleClickOutside(event) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        cb();
-      }
-    }
-
-    // Bind the event listener
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref, cb]);
-};
+import { useOutsideHandle } from '../../hooks';
 
 const DetectClick = (props) => {
   const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef, props.cb);
+  useOutsideHandle(wrapperRef, props.cb);
 
   return <div ref={wrapperRef}>{props.children}</div>;
 }
---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useOutsideHandle } from './useOutsideHandle';
